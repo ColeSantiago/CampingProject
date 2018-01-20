42d2e023e209aa122c650297865713e35449e8c2
Ryan Retirado <ryanretirado@Maes-MacBook-Pro.local>
2018-01-20 13:37:50 -0500

    updated html and css

--- assets/javascript/camping.js
+++ assets/javascript/camping.js
@@ -1,3 +1,30 @@
+//fixed navbar
+
+	$(document).ready(function() {
+		// grab the initial top offset of the navigation 
+		var stickyNavTop = $('#menu').offset().top;
+		   	
+		// our function that decides weather the navigation bar should have "fixed" css position or not.
+		var stickyNav = function(){
+			var scrollTop = $(window).scrollTop(); // our current vertical position from the top
+			         
+			// if we've scrolled more than the navigation, change its position to fixed to stick to top,
+			// otherwise change it back to relative
+			if (scrollTop > stickyNavTop) { 
+				$('#menu').addClass('stickynavbar');
+			} 
+			else {
+				$('#menu').removeClass('stickynavbar'); 
+			}
+		};
+
+			stickyNav();
+			// and run it again every time you scroll
+			$(window).scroll(function() {
+				stickyNav();
+			});
+		});
+
 // array of states and abbrivs
 	const stateArr = [
 	{state:"Alabama", abbr: "AL"}, {state:"Alaska", abbr: "AK"},{state:"Arizona", abbr: "AZ"},{state:"Arkansas", abbr: "AR"},
@@ -74,11 +101,8 @@
 
     })
    })
+
 	
-	//save for later allows to show allows to clear the page without changing files
-	// $('#home').on('click', function(){
-	// 	$('#wholeContainer').show();
-	// })
 
 	
 
