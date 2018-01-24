24a21bba41a01856d6ac1541332107eeb0c61fd6
Cole Santiago <colesantiago@Coles-MacBook-Pro.local>
2018-01-23 20:50:05 -0500

    added css

--- assets/javascript/camping.js
+++ assets/javascript/camping.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+		$(".arrow").hide();
+		
 						// map on load
 
 						mapboxgl.accessToken = 'pk.eyJ1IjoiYnJvd25jb2F0IiwiYSI6ImNqY2Nvb3NibjBpbWIyeW50NHZ6cGZmODUifQ.tAp8DhP9budvHomRqyv0lg';
@@ -50,7 +52,7 @@ $(document).ready(function() {
 						    }
 
 						}
-						
+
 						};
 
 						// map plus minus button
@@ -77,42 +79,56 @@ $(document).ready(function() {
 
 	//looping through and displaying each state in the above array as well as
 	//giving it the value of abbr
-	for (let i = 0; i < stateArr.length; i++){
+ function stateDiv(){
+    for (let i = 0; i < stateArr.length; i++){
+        let newDiv = $('<div class="stateDiv">');
+        let abbr = stateArr[i].abbr;
+        // let arrowDiv = $('<div>');
+        newDiv.html('<button value="'+stateArr[i].abbr+'" class="stateName">' + stateArr[i].state + '</button>');
+        // arrowDiv.html("<span class='glyphicon glyphicon-arrow-left'>")
+        $("#container").append(newDiv);
+        // $(newDiv).prepend(arrowDiv);
+        }
+    }
+    stateDiv();
+    stateName();
+     $('.arrow').on('click',function(){
+        $('.arrow').hide();
+        $('#container').empty();
+        stateDiv();
+        stateName();
+    });
+    //with the value abbr we can then access the api when the user clicks
+    //without any input and we return the response
+    function stateName(){$(".stateName").on('click', function(){
+        $('.arrow').fadeIn('slow');
+        let abbr = $(this).val();
 
-		let newDiv = $('<div class="stateDiv">');
+        const clickedElement = this;
+        $('.stateName').each(function() {
+        if(this !== clickedElement) {
+        $(this).hide();    
+        }});
+        console.log(abbr);
 
-		let abbr = stateArr[i].abbr;
+        $(this).animate({
+            opacity: 1,
+            height: "50px",
+        },1000,"linear");
 
-		newDiv.html('<button value="'+stateArr[i].abbr+'" class="stateName">' + stateArr[i].state + '</button>');
+        $('.stateDiv').css({"display":"inline", "margin":"0px"});
 
-		// newDiv.attr("value", 'stateArr[i].abbr');
+        // $("#container").css("overflow", "hidden");
 
-		$("#container").append(newDiv);
-	}
-
-	//with the value abbr we can then access the api when the user clicks
-	//without any input and we return the response
-	$(".stateName").on('click', function(){
-
-		let campName = $('#camp-name');
-		campName.empty();
-		
-		let abbr = $(this).val();
-
-		console.log(abbr);
-
-
-	const queryURL = "https://api.amp.active.com/camping/campgrounds/?pstate=" + abbr
-	+ "&json=true&api_key=8av4h3s7ecqejs3gbhcj6q6h";
-	const proxyUrl = 'https://shielded-hamlet-43668.herokuapp.com/';
-
+        const queryURL = "https://api.amp.active.com/camping/campgrounds/?pstate=" + abbr
+            + "&json=true&api_key=8av4h3s7ecqejs3gbhcj6q6h";
+        const proxyUrl = 'https://shielded-hamlet-43668.herokuapp.com/';
     $.ajax({
-    	url: proxyUrl + queryURL,
-    	method: "GET",
-    	headers:{
-    		authorization: 'Bearer ' + '8av4h3s7ecqejs3gbhcj6q6h'
-
-    	}
+        url: proxyUrl + queryURL,
+        method: "GET",
+        headers:{
+            authorization: 'Bearer ' + '8av4h3s7ecqejs3gbhcj6q6h'
+        }
     }).done(response => {
     	
     	window.res = response;
@@ -120,65 +136,89 @@ $(document).ready(function() {
 
     		let name = $(res).find('result').each(function(r) { 
 
-    		let nameTwo = $(this).attr('facilityName');
+	    		let nameTwo = $(this).attr('facilityName');
 
-    		let lat = $(this).attr('latitude');
-    		// console.log(lat);
-    		let long = $(this).attr('longitude');
-    		// console.log(long);
-    		const imgURL = 'http://www.reserveamerica.com/' + $(this).attr('faciltyPhoto');
-    		    		
-    		let facilityId = $(this).attr('facilityID');
-    		
-    		let contractId = $(this).attr('contractID');
+	    		let lat = $(this).attr('latitude');
+	    		// console.log(lat);
+	    		let long = $(this).attr('longitude');
+	    		// console.log(long);
+	    		const imgURL = 'http://www.reserveamerica.com/' + $(this).attr('faciltyPhoto');
+	    		    		
+	    		let facilityId = $(this).attr('facilityID');
+	    		
+	    		let contractId = $(this).attr('contractID');
 
-    		let campArray = [];
-    		let longArray = [];
-    		let latArray = [];
-    		let facilityArray = [];
-    		let contractArray = [];
-    		let photoArray = [];
+	    		let campArray = [];
+	    		let longArray = [];
+	    		let latArray = [];
+	    		let facilityArray = [];
+	    		let contractArray = [];
+	    		let photoArray = [];
 
-    		longArray.push(long);
-    		latArray.push(lat);
-    		campArray.push(nameTwo);
-    		facilityArray.push(facilityId);
-    		contractArray.push(contractId);
-    		photoArray.push(imgURL);
-    		// console.log(photoArray);
+	    		const campButtons = $('<button class="camp-button"><a></a></button>');
+
+	    		longArray.push(long);
+	    		latArray.push(lat);
+	    		campArray.push(nameTwo);
+	    		facilityArray.push(facilityId);
+	    		contractArray.push(contractId);
+	    		photoArray.push(imgURL);
 
     		// adding camp buttons to the html when the state is clicked
-    		for (let i = 0; i < campArray.length; i++) {
+    		$('#container').append($("<div id='buttonContainer'>"));
+                // adding to the html
+                let newLine = $('<br>');
+                for (let i = 0; i < campArray.length; i++) {
+                    // console.log(campArray.length);
+                    // campButtons.attr('href', site);
+                    campButtons.attr('data-camp', campArray[i]);
+                    campButtons.val(abbr);
+                    campButtons.attr('data-lat', lat);
+                    console.log(lat);
+                    campButtons.attr('data-long', long);
+                    console.log(long);
+                    campButtons.attr('data-facility', facilityArray[i]);
+                    campButtons.attr('data-contract', contractArray[i]);
+                    campButtons.text(campArray[i]);
+                    campButtons.attr('data-photo', photoArray[i]);
+                    $('#buttonContainer').append(campButtons);
+                    }
+                 })
+    		// for (let i = 0; i < campArray.length; i++) {
 
-	    		const campButtons = $('<button>');
+	    	// 	// const campButtons = $('<button>');
 
-	    		campButtons.addClass('camp-button');
+	    	// 	campButtons.addClass('camp-button');
 
-	    		campButtons.attr('data-camp', campArray[i]);
+	    	// 	campButtons.attr('data-camp', campArray[i]);
 
-	    		campButtons.attr('data-long', longArray[i]);
+	    	// 	campButtons.attr('data-long', longArray[i]);
 	    		
-	    		campButtons.attr('data-lat', latArray[i]);
+	    	// 	campButtons.attr('data-lat', latArray[i]);
 
-	    		campButtons.attr('data-facility', facilityArray[i]);
+	    	// 	campButtons.attr('data-facility', facilityArray[i]);
 
-	    		campButtons.attr('data-contract', contractArray[i]);
+	    	// 	campButtons.attr('data-contract', contractArray[i]);
 
-	    		campButtons.attr('data-photo', photoArray[i]);
+	    	// 	campButtons.attr('data-photo', photoArray[i]);
 	    		
-	    		campButtons.text(campArray[i]);
+	    	// 	campButtons.text(campArray[i]);
 
-	    		campName.append(campButtons);
+	    	// 	campName.append(campButtons);
 
-	    	}
+	    	// }
 
 	    })
 
     })
 
-})
+}
+
+
 
 			$(document).on('click', '.camp-button', function(){ 
+
+				$('.camp-button').hide();
 
 						// map when you select a campsite
 
@@ -228,40 +268,40 @@ $(document).ready(function() {
 				        method: "GET",
 				    	}).done(response => {
 				    		for (let i = 0; i < response.trails.length; i++){
-				    	let trailLat = response.trails[i].latitude;
-				    	let trailLong = response.trails[i].longitude;
-				    	let trailName = response.trails[i].name;
-				    	const url = response.trails[i].url;
-				    	const iframe = $("<iframe height='400px' width='400px'>");
-				    	const link = $(iframe).attr('src', url);
-				    	$('.trailsDiv').append(link);
+						    	let trailLat = response.trails[i].latitude;
+						    	let trailLong = response.trails[i].longitude;
+						    	let trailName = response.trails[i].name;
+						    	// const url = response.trails[i].url;
+						    	// const iframe = $("<iframe height='400px' width='400px'>");
+						    	// const link = $(iframe).attr('src', url);
+						    	// $('.trailsDiv').append(link);
    	
-   							// adds trail locations to the map
-					    	let trailImg = new Image('id="trailDot');
-							trailImg.src = 'assets/images/dot.png';
-							trailImg.style.height = '20px';
-				    		trailImg.style.width = '20px';
-				    		trailImg.style.borderRadius = '25px';	
+	   							// adds trail locations to the map
+						    	let trailImg = new Image('id="trailDot');
+								trailImg.src = 'assets/images/dot.png';
+								trailImg.style.height = '20px';
+					    		trailImg.style.width = '20px';
+					    		trailImg.style.borderRadius = '25px';	
 
-							let marker = new mapboxgl.Marker(trailImg)
-							.setLngLat([trailLong, trailLat])
-							.addTo(map)
-							
-							var markerHeight = 10, markerRadius = 10, linearOffset = 25;
-							var popupOffsets = {
-							 'top': [0, 0],
-							 'top-left': [0,0],
-							 'top-right': [0,0],
-							 'bottom': [0, -markerHeight],
-							 'bottom-left': [linearOffset, (markerHeight - markerRadius + linearOffset) * -1],
-							 'bottom-right': [-linearOffset, (markerHeight - markerRadius + linearOffset) * -1],
-							 'left': [markerRadius, (markerHeight - markerRadius) * -1],
-							 'right': [-markerRadius, (markerHeight - markerRadius) * -1]
-							 };
-							var popup = new mapboxgl.Popup({offset:popupOffsets})
-							  .setLngLat([trailLong, trailLat])
-							  .setHTML('Trail: ' + trailName)
-							  .addTo(map);
+								let marker = new mapboxgl.Marker(trailImg)
+								.setLngLat([trailLong, trailLat])
+								.addTo(map)
+								
+								var markerHeight = 10, markerRadius = 10, linearOffset = 25;
+								var popupOffsets = {
+								 'top': [0, 0],
+								 'top-left': [0,0],
+								 'top-right': [0,0],
+								 'bottom': [0, -markerHeight],
+								 'bottom-left': [linearOffset, (markerHeight - markerRadius + linearOffset) * -1],
+								 'bottom-right': [-linearOffset, (markerHeight - markerRadius + linearOffset) * -1],
+								 'left': [markerRadius, (markerHeight - markerRadius) * -1],
+								 'right': [-markerRadius, (markerHeight - markerRadius) * -1]
+								 };
+								var popup = new mapboxgl.Popup({offset:popupOffsets})
+								  .setLngLat([trailLong, trailLat])
+								  .setHTML('Trail: ' + trailName)
+								  .addTo(map);
 
 						// end map
 						// end trail api
@@ -298,60 +338,60 @@ $(document).ready(function() {
 					    		const weatherDescription0 = $('<p>').text(weatherResponse.list[0].weather[0].description);
 					    		weatherDiv.append(weatherDescription0);
 
-					    		// day 2 forecast
-					    		weatherDate8 = $('<p>').text('Date: ' + weatherResponse.list[8].dt_txt);
-					    		weatherDiv.append(weatherDate8);
+					    		// // day 2 forecast
+					    		// weatherDate8 = $('<p>').text('Date: ' + weatherResponse.list[8].dt_txt);
+					    		// weatherDiv.append(weatherDate8);
 
-					    		let tempFar8 = weatherResponse.list[8].main.temp * 9/5 - 459.67;
-					    		let tempFarRounded8 = Math.floor(tempFar8);
+					    		// let tempFar8 = weatherResponse.list[8].main.temp * 9/5 - 459.67;
+					    		// let tempFarRounded8 = Math.floor(tempFar8);
 
-					    		const weatherMain8 = $('<p>').text('Temperature: ' + tempFarRounded8 + ' degrees F');
-					    		weatherDiv.append(weatherMain8);
+					    		// const weatherMain8 = $('<p>').text('Temperature: ' + tempFarRounded8 + ' degrees F');
+					    		// weatherDiv.append(weatherMain8);
 
-					    		const weatherDescription8 = $('<p>').text(weatherResponse.list[8].weather[0].description);
-					    		weatherDiv.append(weatherDescription8);
+					    		// const weatherDescription8 = $('<p>').text(weatherResponse.list[8].weather[0].description);
+					    		// weatherDiv.append(weatherDescription8);
 
-					    		// day 3 forecast
-					    		const weatherDate16 = $('<p>').text('Date: ' + weatherResponse.list[16].dt_txt)
-					    		weatherDiv.append(weatherDate16);
+					    		// // day 3 forecast
+					    		// const weatherDate16 = $('<p>').text('Date: ' + weatherResponse.list[16].dt_txt)
+					    		// weatherDiv.append(weatherDate16);
 
-					    		let tempFar16 = weatherResponse.list[16].main.temp * 9/5 - 459.67;
-					    		let tempFarRounded16 = Math.floor(tempFar16);
+					    		// let tempFar16 = weatherResponse.list[16].main.temp * 9/5 - 459.67;
+					    		// let tempFarRounded16 = Math.floor(tempFar16);
 
-					    		const weatherMain16 = $('<p>').text('Temperature: ' + tempFarRounded16 + ' degrees F');
-					    		weatherDiv.append(weatherMain16);
+					    		// const weatherMain16 = $('<p>').text('Temperature: ' + tempFarRounded16 + ' degrees F');
+					    		// weatherDiv.append(weatherMain16);
 
-					    		const weatherDescription16 = $('<p>').text(weatherResponse.list[16].weather[0].description);
-					    		weatherDiv.append(weatherDescription16);
+					    		// const weatherDescription16 = $('<p>').text(weatherResponse.list[16].weather[0].description);
+					    		// weatherDiv.append(weatherDescription16);
 
-					    		// day 4 forecast
-					    		const weatherDate24 = $('<p>').text('Date: ' + weatherResponse.list[24].dt_txt)
-					    		weatherDiv.append(weatherDate24);
+					    		// // day 4 forecast
+					    		// const weatherDate24 = $('<p>').text('Date: ' + weatherResponse.list[24].dt_txt)
+					    		// weatherDiv.append(weatherDate24);
 
-					    		let tempFar24 = weatherResponse.list[24].main.temp * 9/5 - 459.67;
-					    		let tempFarRounded24 = Math.floor(tempFar24);
+					    		// let tempFar24 = weatherResponse.list[24].main.temp * 9/5 - 459.67;
+					    		// let tempFarRounded24 = Math.floor(tempFar24);
 
-					    		const weatherMain24 = $('<p>').text('Temperature: ' + tempFarRounded24 + ' degrees F');
-					    		weatherDiv.append(weatherMain24);
+					    		// const weatherMain24 = $('<p>').text('Temperature: ' + tempFarRounded24 + ' degrees F');
+					    		// weatherDiv.append(weatherMain24);
 
-					    		const weatherDescription24 = $('<p>').text(weatherResponse.list[24].weather[0].description);
-					    		weatherDiv.append(weatherDescription24);
+					    		// const weatherDescription24 = $('<p>').text(weatherResponse.list[24].weather[0].description);
+					    		// weatherDiv.append(weatherDescription24);
 
-					    		// day 5 forecast
-					    		const weatherDate32 = $('<p>').text('Date: ' + weatherResponse.list[32].dt_txt)
-					    		weatherDiv.append(weatherDate32);
+					    		// // day 5 forecast
+					    		// const weatherDate32 = $('<p>').text('Date: ' + weatherResponse.list[32].dt_txt)
+					    		// weatherDiv.append(weatherDate32);
 
-					    		let tempFar32 = weatherResponse.list[32].main.temp * 9/5 - 459.67;
-					    		let tempFarRounded32 = Math.floor(tempFar32);
+					    		// let tempFar32 = weatherResponse.list[32].main.temp * 9/5 - 459.67;
+					    		// let tempFarRounded32 = Math.floor(tempFar32);
 					    		
-					    		const weatherMain32 = $('<p>').text('Temperature: ' + tempFarRounded32 + ' degrees F');
-					    		weatherDiv.append(weatherMain32);
+					    		// const weatherMain32 = $('<p>').text('Temperature: ' + tempFarRounded32 + ' degrees F');
+					    		// weatherDiv.append(weatherMain32);
 
-					    		const weatherDescription32 = $('<p>').text(weatherResponse.list[32].weather[0].description);
-					    		weatherDiv.append(weatherDescription32);
+					    		// const weatherDescription32 = $('<p>').text(weatherResponse.list[32].weather[0].description);
+					    		// weatherDiv.append(weatherDescription32);
 
 					    		$('#weather-view').empty();
-					    		$('#weather-view').prepend(weatherDiv);
+					    		$('#buttonContainer').prepend(weatherDiv);
 
 
 				    	})
@@ -384,32 +424,29 @@ $(document).ready(function() {
 
 					    					let detailAddress = $(res).find('address').each(function(r) {
 
-					    						const city = $('<p>').text($(this).attr('city'));
-					    						detailDiv.append(city);
+					    						const city = $('<p class="city">').text($(this).attr('city'));
+                                                detailDiv.append(city);
 
-					    						const state = $('<p>').text($(this).attr('state'));
-					    						detailDiv.append(state);
+                                                const address = $('<p class="street">').text($(this).attr('streetAddress'));
+                                                detailDiv.append(address);
 
-							    				const address = $('<p>').text($(this).attr('streetAddress'));
-							    				detailDiv.append(address);
+                                                const zipCode = $('<p class="zip">').text($(this).attr('zip'));
+                                                detailDiv.append(zipCode);
 
-							    				const zipCode = $('<p>').text($(this).attr('zip'));
-							    				detailDiv.append(zipCode);
+                                            })
 
-							    			})
+                                        const detailTwo = $('<p class="main-info">').text($(this).attr('description'));
+                                        detailDiv.append(detailTwo);
 
-						    			const detailTwo = $('<p>').text($(this).attr('description'));
-						    			detailDiv.append(detailTwo);
+                                        const recreation = $('<p class="rect-info">').text($(this).attr('recreationDescription'));
+                                        detailDiv.append(recreation);
 
-						    			const recreation = $('<p>').text($(this).attr('recreationDescription'));
-						    			detailDiv.append(recreation);
-
-						    			const important = $('<p>').text($(this).attr('importantInformation'));
-						    			detailDiv.append(important);
+                                        const important = $('<p class="important-info">').text($(this).attr('importantInformation'));
+                                        detailDiv.append(important);
 
 						    			const reservation = $('<a>').text('Make a Reservation');
 						    			reservation.attr('href', $(this).attr('fullReservationUrl'));
-						    			window.open(reservation, '_blank');
+						    			reservation.attr('target', '_blank');
 						    			detailDiv.append(reservation);
 						    			console.log(reservation);
 
@@ -417,7 +454,7 @@ $(document).ready(function() {
 						    			detailDiv.append(alert);
 
 						    			$('#detail-view').empty();
-						    			$('#detail-view').prepend(detailDiv);
+						    			$('#buttonContainer').append(detailDiv);
 
 
 
