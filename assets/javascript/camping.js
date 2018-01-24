c51ab9ea5811ec2c59fdd38b36d93f922ea0c6f8
Cole Santiago <colesantiago@Coles-MacBook-Pro.local>
2018-01-24 05:41:45 -0500

    added trail bootstrap and prep div. prep div still needs to be fixed.

--- assets/javascript/camping.js
+++ assets/javascript/camping.js
@@ -1,64 +1,149 @@
 $(document).ready(function() {
 
+	//Modal Image Code
+
+	// Get the modal
+	let modal = document.getElementById('myModal');
+	let modalImg = document.getElementById("img01");
+	let captionText = document.getElementById("caption");
+
+//Image Objects
+	const tipCards = {
+	  'backpack': {
+	    longImage: 'assets/images/camp-prep-1-long.jpg',
+	  },
+	  'bear': {
+	    longImage: 'assets/images/camp-prep-2-long.png',
+	  },
+	  'mosquito': {
+	  	longImage: 'assets/images/camp-prep-3-long.jpg',
+	  },
+	  'hypothermia': {
+	  	longImage: 'assets/images/camp-prep-4-long.jpg',
+	  },
+	  'spiderbites': {
+	  	longImage: 'assets/images/camp-prep-5-long.jpg',
+	  },
+	  'campfire': {
+	  	longImage: 'assets/images/camp-prep-6-long.jpg',
+	  },
+	  'first-aid': {
+	  	longImage: 'assets/images/camp-prep-7-long.jpg',
+	  },
+	  'shelter': {
+	  	longImage: 'assets/images/camp-prep-8-long.jpg',
+	  },
+	  'tent': {
+	  	longImage: 'assets/images/camp-prep-9-long.png',
+	  },
+	  'trapping': {
+	  	longImage: 'assets/images/camp-prep-10-long.jpg',
+	  },
+	};
+
+// Opens modal and calls on object's longImage property.
+	$(".tip-card").on("click", function() {
+	  const id = document.getElementById(this.id);
+	  let tipCardsItems = tipCards[this.id];
+	// Console logging object info
+	  console.log(id);
+	  console.log(tipCardsItems);
+
+	  modal.style.display = "block";
+	  const modalImage = document.querySelector('.w3s-modal-content');
+	  modalImage.src = tipCards[this.id].longImage;
+	  captionText.innerHTML = this.alt;
+
+	  });
+
+	// Get the <span> element that closes the modal
+	let span = document.getElementsByClassName("close")[0];
+
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() { 
+	  modal.style.display = "none";
+	}
+
 		$(".arrow").hide();
-		
+
+		// grab the initial top offset of the navigation 
+        var stickyNavTop = $('#menu').offset().top;
+               
+        // our function that decides weather the navigation bar should have "fixed" css position or not.
+        var stickyNav = function(){
+            var scrollTop = $(window).scrollTop(); // our current vertical position from the top
+                     
+            // if we've scrolled more than the navigation, change its position to fixed to stick to top,
+            // otherwise change it back to relative
+            if (scrollTop > stickyNavTop) { 
+                $('#menu').addClass('stickynavbar');
+            } 
+            else {
+                $('#menu').removeClass('stickynavbar'); 
+            }
+        };
+
+            stickyNav();
+            // and run it again every time you scroll
+            $(window).scroll(function() {
+                stickyNav();
+            });
+
 						// map on load
 
-						mapboxgl.accessToken = 'pk.eyJ1IjoiYnJvd25jb2F0IiwiYSI6ImNqY2Nvb3NibjBpbWIyeW50NHZ6cGZmODUifQ.tAp8DhP9budvHomRqyv0lg';
-						var map = new mapboxgl.Map({
-						container: 'map',
-						style: 'mapbox://styles/mapbox/outdoors-v10',
-						center: [-103.579102, 48.365374],
-						zoom: 2
-						});
+			mapboxgl.accessToken = 'pk.eyJ1IjoiYnJvd25jb2F0IiwiYSI6ImNqY2Nvb3NibjBpbWIyeW50NHZ6cGZmODUifQ.tAp8DhP9budvHomRqyv0lg';
+			var map = new mapboxgl.Map({
+				container: 'map',
+				style: 'mapbox://styles/mapbox/outdoors-v10',
+				center: [-103.579102, 48.365374],
+				zoom: 2
+			});
 
+			// adds hillshading to the map
+			map.on('load', function () {
+				map.addSource('dem', {
+				"type": "raster-dem",
+				"url": "mapbox://mapbox.terrain-rgb"
+			});
+			map.addLayer({
+				"id": "hillshading",
+				"source": "dem",
+				"type": "hillshade"
+				}, 'waterway-river-canal-shadow');
+			});
 
-						// adds hillshading to the map
-						  map.on('load', function () {
-						    map.addSource('dem', {
-						        "type": "raster-dem",
-						        "url": "mapbox://mapbox.terrain-rgb"
-						    });
-						    map.addLayer({
-						        "id": "hillshading",
-						        "source": "dem",
-						        "type": "hillshade"
-						    }, 'waterway-river-canal-shadow');
-						});
-
-						  // finds the users location
-						map.addControl(new mapboxgl.GeolocateControl({
-						    positionOptions: {
-						        enableHighAccuracy: true
-						    },
-						    trackUserLocation: true
-						}));
+			// finds the users location
+			map.addControl(new mapboxgl.GeolocateControl({
+				positionOptions: {
+					enableHighAccuracy: true
+				},
+					trackUserLocation: true
+			}));
 	
-						// adds directions section
-						map.addControl(new MapboxDirections({
-						    accessToken: mapboxgl.accessToken
-						}), 'top-left');
+			// adds directions section
+			map.addControl(new MapboxDirections({
+				accessToken: mapboxgl.accessToken
+			}), 'top-left');
 
-						// scrolls to the location of your mouse on the map
-						if (map.mouseenter) {
-						  window.onscroll = function() {
-						    var chapterNames = Object.keys(chapters);
-						    for (var i = 0; i < chapterNames.length; i++) {
-						        var chapterName = chapterNames[i];
-						        if (isElementOnScreen(chapterName)) {
-						            setActiveChapter(chapterName);
-						            break;
-						        }
-						    }
+			// scrolls to the location of your mouse on the map
+			if (map.mouseenter) {
+				window.onscroll = function() {
+					var chapterNames = Object.keys(chapters);
+						for (var i = 0; i < chapterNames.length; i++) {
+						     var chapterName = chapterNames[i];
+						     if (isElementOnScreen(chapterName)) {
+						        setActiveChapter(chapterName);
+						        break;
+						     }
+					}
 
-						}
+				}
 
-						};
+			};
 
-						// map plus minus button
-						map.addControl(new mapboxgl.NavigationControl());
-
-					// end map  
+			// map plus minus button
+			map.addControl(new mapboxgl.NavigationControl());
+			// end map  
 
 // array of states and abbrivs
 	const stateArr = [
@@ -77,6 +162,83 @@ $(document).ready(function() {
 	{state:"Wisconsin", abbr: "WI"},{state:"Wyoming", abbr: "WY"}
 	]
 
+	 const nonProfits = [
+        {"Name": "Wildlife Conservation Network", 
+         "Description":"This eco-friendly non-profit organization protects endangered species and preserves their natural habitats.  WCN partners with independent, community-based conservationists around the world and gives them the capital and tools they need to develop solutions for human-wildlife coexistence.  100% of any donation can be designated to the conservation of a specific species if requested.", 
+         "Poster": "https://animaliacollective.com/wp-content/uploads/2017/03/wcn_partner-logo.png", 
+         "pageLink":"https://wildnet.org/"
+        },
+        {"Name":"The Nature Conservancy",
+         "Description":"From climate change and rainforests to anything in between, this eco-friendly, non-profit organization is committed to taking on tough issues facing conservation efforts today. To date, they have protected more than 119 million acres of land and 5,000 miles of rivers worldwide, making it one of the largest eco-oriented charities in the world.",
+         "Poster":"https://www.nature.org/cs/groups/webasset/documents/webasset/tnc-share-logo.gif",
+         "pageLink":"https://www.nature.org/?redirect=https-301"
+        },
+        {"Name":"Environmental Defense Fund",
+         "Description":"This eco-friendly non-profit takes on most urgent environmental threats to the climate, ecosystems, people’s health, and more and then fights them by combining science, economics, and law to find practical and lasting solutions.  They scored high on Charity Finder and uses 79% of their funds for program expenses.",
+         "Poster":"http://www.bkv.com/assets/clients/environmental_defense_fund-86dd956352770392146bb25ddf8f1916f0ec98a48c14d76648bff47b92214be2.png",
+         "pageLink":"https://www.edf.org/"
+        },
+        {"Name":"Greenpeace",
+         "Description":"This world renowned eco-friendly organization uses lobbying and research to reach its environmental protection and conservation goals. From protecting oceans, forests, and wildlife to working towards no new nukes, no issue is too tough for Greenpeace to go after.",
+         "Poster":"https://financialtribune.com/sites/default/files/field/image/13_Greenpeace.jpg",
+         "pageLink":"http://www.greenpeace.org/usa/"
+        },
+        {"Name":"National Geographic Society",
+         "Description":"The key to this eco-friendly organization’s conservation approach is to show the planet’s natural beauty so people will appreciate it. In addition to conservation, National Geographic supports exploration and scientific research through grant programs and public projects.",
+         "Poster":"https://impactinvestingconferences.com/wp-content/uploads/2017/06/NGS.png",
+         "pageLink":"https://donate.nationalgeographic.org/17-ge-adw?gclid=Cj0KCQiA-ebSBRC8ARIsAGuxJIpoDl4PXsTKPj_FiOKOfsPA5aBLFrGyoRORfUX09i9Les4pDng7z9AaAiTeEALw_wcB"
+        }
+    ]
+
+    //for loop creating divs and adding information from nonProfits array into each div
+    for (let i = 0; i < nonProfits.length; i++) {
+        
+        //main container for each non-profit
+        const nonProfitContainer = $('<div>');
+        nonProfitContainer.addClass('non-profit-container');
+
+        //div containing name of each non-profit
+        const nameDiv = $('<div>');
+        nameDiv.addClass('charity-name');
+        nameDiv.text(nonProfits[i].Name);
+
+        //div containing images of each non-profit's logo
+        const posterDiv = $('<img>');
+        posterDiv.addClass('charity-poster');
+        posterDiv.attr("src", nonProfits[i].Poster);
+
+        //div containing each non-profits descriptions/mission statements
+        const descriptionDiv = $('<div>');
+        descriptionDiv.addClass('charity-description');
+        descriptionDiv.text(nonProfits[i].Description);
+
+        //div containing a tags linking users to each non-profits' pages
+        const donateDiv = $('<div>');
+        const donateBtn = $('<a>' + "Learn More & Donate!" + '</a>');
+        $(donateDiv).html("<p>");
+        donateBtn.attr("href", nonProfits[i].pageLink);
+        donateBtn.attr("target", "_blank");
+        donateBtn.attr("data-value", "Learn More & Donate!");
+        donateDiv.addClass("donate-button");
+        $(donateDiv).append(donateBtn);
+
+        //appending divs to main container
+        $(nonProfitContainer).append(nameDiv);
+        $(nonProfitContainer).append(posterDiv);
+        $(nonProfitContainer).append(descriptionDiv);
+        $(nonProfitContainer).append(donateDiv);
+
+        $('#donate-hold').append(nonProfitContainer);
+
+    };
+
+    $("#donate-link").on("click", function() {
+        $("#mainBodyDiv").hide();
+        $("#container").hide();
+        $("#prep").hide();
+    })
+
+
 	//looping through and displaying each state in the above array as well as
 	//giving it the value of abbr
  function stateDiv(){
@@ -88,19 +250,23 @@ $(document).ready(function() {
         // arrowDiv.html("<span class='glyphicon glyphicon-arrow-left'>")
         $("#container").append(newDiv);
         // $(newDiv).prepend(arrowDiv);
-        }
-    }
+     }
+  }
     stateDiv();
     stateName();
      $('.arrow').on('click',function(){
         $('.arrow').hide();
         $('#container').empty();
+        $('#carouselContainer').css('display', 'none');
         stateDiv();
         stateName();
     });
     //with the value abbr we can then access the api when the user clicks
     //without any input and we return the response
     function stateName(){$(".stateName").on('click', function(){
+
+    	$('#buttonContainer').empty();
+
         $('.arrow').fadeIn('slow');
         let abbr = $(this).val();
 
@@ -174,51 +340,27 @@ $(document).ready(function() {
                     campButtons.attr('data-camp', campArray[i]);
                     campButtons.val(abbr);
                     campButtons.attr('data-lat', lat);
-                    console.log(lat);
+                    // console.log(lat);
                     campButtons.attr('data-long', long);
-                    console.log(long);
+                    // console.log(long);
                     campButtons.attr('data-facility', facilityArray[i]);
                     campButtons.attr('data-contract', contractArray[i]);
                     campButtons.text(campArray[i]);
                     campButtons.attr('data-photo', photoArray[i]);
+
                     $('#buttonContainer').append(campButtons);
                     }
                  })
-    		// for (let i = 0; i < campArray.length; i++) {
-
-	    	// 	// const campButtons = $('<button>');
-
-	    	// 	campButtons.addClass('camp-button');
-
-	    	// 	campButtons.attr('data-camp', campArray[i]);
-
-	    	// 	campButtons.attr('data-long', longArray[i]);
-	    		
-	    	// 	campButtons.attr('data-lat', latArray[i]);
-
-	    	// 	campButtons.attr('data-facility', facilityArray[i]);
-
-	    	// 	campButtons.attr('data-contract', contractArray[i]);
-
-	    	// 	campButtons.attr('data-photo', photoArray[i]);
-	    		
-	    	// 	campButtons.text(campArray[i]);
-
-	    	// 	campName.append(campButtons);
-
-	    	// }
-
 	    })
 
     })
 
 }
 
-
-
 			$(document).on('click', '.camp-button', function(){ 
 
 				$('.camp-button').hide();
+				$('#carouselContainer').css('display', 'block');
 
 						// map when you select a campsite
 
@@ -261,20 +403,24 @@ $(document).ready(function() {
 
 					// trail api
 					const trailsqueryURL = "https://www.hikingproject.com/data/get-trails?lat=" + $(this).attr('data-lat') + "&lon=" + $(this).attr('data-long') + 
-				    "&maxDistance=5&maxResults=5&key=200209593-2d1e8288276f62fa07701a4f0905a28f"
+				    "&maxDistance=10&maxResults=5&key=200209593-2d1e8288276f62fa07701a4f0905a28f"
 
 				     $.ajax({
 				        url: trailsqueryURL,
 				        method: "GET",
 				    	}).done(response => {
 				    		for (let i = 0; i < response.trails.length; i++){
+                        		const url = response.trails[i].url;
+
+                        		$('#slide'+ (i + 1)).attr("src", url);
+                            }
+                            
+                            // $('.carousel').carousel({interval: 1000})
+                
+				    		for (let i = 0; i < response.trails.length; i++){
 						    	let trailLat = response.trails[i].latitude;
 						    	let trailLong = response.trails[i].longitude;
 						    	let trailName = response.trails[i].name;
-						    	// const url = response.trails[i].url;
-						    	// const iframe = $("<iframe height='400px' width='400px'>");
-						    	// const link = $(iframe).attr('src', url);
-						    	// $('.trailsDiv').append(link);
    	
 	   							// adds trail locations to the map
 						    	let trailImg = new Image('id="trailDot');
@@ -322,8 +468,6 @@ $(document).ready(function() {
 				    		// console.log(weatherResponse);
 
 				    		const weatherDiv = $("<div class='weather'>");
-
-
 
 				    			// day 1 forecast
 					    		const weatherDate0 = $('<p>').text('Date: ' + weatherResponse.list[0].dt_txt);
@@ -393,7 +537,6 @@ $(document).ready(function() {
 					    		$('#weather-view').empty();
 					    		$('#buttonContainer').prepend(weatherDiv);
 
-
 				    	})
  	
 				    	// end weather api
@@ -456,8 +599,6 @@ $(document).ready(function() {
 						    			$('#detail-view').empty();
 						    			$('#buttonContainer').append(detailDiv);
 
-
-
 					    		})
 
 					    	})			
@@ -469,7 +610,6 @@ $(document).ready(function() {
 	})
 
 
-
-	
 // closing tag for document ready
 });
+
